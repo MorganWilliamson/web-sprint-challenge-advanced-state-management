d3849b6652521829c05d021a423b8ac2be0132a5
MorganWilliamson <mwilliamson7695@gmail.com>
2020-11-06 10:53:01 -0600

    Setting up reducers, actions.

--- smurfs/src/store/actions/index.js
+++ smurfs/src/store/actions/index.js
@@ -1,1 +1,1 @@
-//index.js within the actions directory. 
+////////// index.js within the actions directory ////////// 
--- smurfs/src/store/reducers/index.js
+++ smurfs/src/store/reducers/index.js
@@ -1,1 +1,37 @@
-//index.js within the reducers directory. 
+////////// index.js within the reducers directory ////////// 
+//import from actions
+
+//initial state
+const initialState = {
+    smurfs: [], 
+    isLoading: false,
+    error: "",
+}
+
+//reducer function
+export function reducer (state = initialState, action) {
+    switch(action.type) {
+        case FETCH_SMURFS_START: 
+            return{
+                ...state, 
+                isLoading: true,
+            };
+
+        case FETCH_SMURFS_SUCCESS: 
+            return {
+                ...state, 
+                isLoading: false, 
+                smurfs: action.payload,
+            };
+
+        case FETCH_SMURFS_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload,
+            }
+
+        default: 
+            return state;
+    }
+}
